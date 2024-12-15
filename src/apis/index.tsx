e24d719a06f0c5066c09c32261953c9d7fb2cc52
kojikoji <1310402980@qq.com>
2024-12-15 14:30:02 +0800

    api

--- src/apis/index.tsx
+++ src/apis/index.tsx
@@ -5,13 +5,27 @@ const host = 'https://apifoxmock.com/m1/2559383-1518511-default'
 
 export const getRoleList = () => {
     // 从 apiPost mock 接口获取
-    return fetch(`${host}/api/roles`).then((res) =>
-        res.json()
-    );
-
-    // // 从本地 json 文件获取
-    // return fetch(`/prompts.json`).then((res) =>
+    // return fetch(`${host}/api/roles`).then((res) =>
     //     res.json()
     // );
+
+    // // 从本地 json 文件获取
+    return fetch(`/prompts.json`).then((res) =>
+        res.json()
+    );
 }
 
+export const completions = (data: {
+    messages: { content: string; role: MessageRole }[],
+    model: GptVersion
+}) => {
+    return fetch('http://124.221.174.50:8080/api/v1/chat/completions', {
+        method: 'post',
+        headers: {
+            // b8b6 后续用于写入 token 加密信息
+            Authorization: "b8b6",
+            'Content-Type': 'application/json;charset=utf-8'
+        },
+        body: JSON.stringify(data)
+    })
+}
--- src/app/constants.ts
+++ src/app/constants.ts
@@ -5,6 +5,21 @@ export enum Path {
 }
 
 export enum GptVersion {
+    // ChatGLM
+    CHATGLM_TURBO = "chatglm_turbo",
+    GLM_3_5_TURBO = "glm-3-turbo",
+    GLM_4 = "glm-4",
+    GLM_4V = "glm-4v",
+    GLM_4_Plus = "glm-4-plus",
+    GLM_4_0520 = "glm-4-0520",
+    GLM_4_Lng = "glm-4-long",
+    GLM_4_AirX = "glm-4-airx",
+    GLM_4_Air = "glm-4-air",
+    GLM_4_FlashX = "glm-4-flashx",
+    GLM_4_Flash = "glm-4-flash",
+    GLM_4_AllTools = "glm-4-alltools",
+    COGVIEW_3 = "cogview-3",
+    // ChatGpt
     TEXT_DAVINCI_003 = "text-davinci-003",
     TEXT_DAVINCI_002 = "text-davinci-002",
     DAVINCI = "davinci",
@@ -13,3 +28,4 @@ export enum GptVersion {
     GPT_4 = "gpt-4",
     GPT_4_32K = "gpt-4-32k",
 }
+
